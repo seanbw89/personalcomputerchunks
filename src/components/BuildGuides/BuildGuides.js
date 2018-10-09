4b1d4f091fcd16f362772c4b1426514a53134cca
unknown <sean.b.white89@icloud.com>
2018-10-09 07:43:48 -0600

    added-maintenece

--- src/components/BuildGuides/BuildGuides.js
+++ src/components/BuildGuides/BuildGuides.js
@@ -15,9 +15,12 @@ const BuildGuides = () => {
       <div style={{width:'100%', background:'rgb(78,94,166)',height:'110px', display:'flex', justifyContent:'flex-start', alignItems:'center'}}>
           <h2 style={{color:'white', fontSize:'2rem', fontFamily:'Lato, sans-serif', marginLeft:'350px'}}>
             Personal Computer Chunks Build Guides
-          </h2>
-        </div>      
-        <ul style={{width:'70%', height:'100%', background:'red', marginLeft:'540px'}}>
+          </h2>      
+      </div>   
+      <div>
+        <h1 style={{fontSize:'3rem', margin:'0 auto'}}>Currently under maintenance</h1>
+      </div>
+        {/* <ul style={{width:'70%', height:'100%', background:'red', marginLeft:'540px'}}>
           <li>
             <div style={{width:'100%', height:'250px', background:'yellow', border:'2px solid black'}}>
 
@@ -28,7 +31,7 @@ const BuildGuides = () => {
 
             </div>
           </li>          
-        </ul>
+        </ul> */}
     </Wrapper>
   )
 }
